bc894a1154e575b32e11a93906d0defbba51b552
kevin1kevin1k <kevin1kevin1k@gmail.com>
2020-07-31 18:36:11 +0800

    Add author

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,5 @@
-chrome.storage.sync.set({domain: document.domain});
+var author = document.getElementsByClassName("author")[0];
+chrome.storage.sync.set({
+  domain: document.domain,
+  author: author.innerHTML
+});
--- popup.js
+++ popup.js
@@ -9,13 +9,22 @@ var map = {
 
 let text1 = document.getElementById('text1');
 let text2 = document.getElementById('text2');
-chrome.storage.sync.get('domain', async (data) => {
+let text3 = document.getElementById('text3');
+let text4 = document.getElementById('text4');
+chrome.storage.sync.get(['domain'], async (data) => {
   let domain = data.domain;
   let url = 'https://media-expose.wancat.cc/api/media?domain=' + domain;
   let response = await fetch(url, {method: 'get'});
   let info = await response.json();
   console.log(info);
   let company = info[0]['Company'];
-  text1.innerHTML = company['Name'];
-  text2.innerHTML = company['Note'];
+  text1.innerHTML = `${company['Name']} (${info[0]['Country']})`;
+  text2.innerHTML = company['Owner'];
+  text3.innerHTML = company['Note'];
+});
+
+chrome.storage.sync.get('author', (data) => {
+  let author = data.author;
+  text4.innerHTML = author;
+  console.log(author);
 });
