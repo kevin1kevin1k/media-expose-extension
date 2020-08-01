61fb7009214002239ded1965d13b407edf830b77
kevin1kevin1k <kevin1kevin1k@gmail.com>
2020-08-01 14:01:55 +0800

    Show empty popup on other domains

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {urlMatches: 'news.ltn.com.tw|tw.appledaily.com|www.chinatimes.com|udn.com|news.tvbs.com.tw|www.setn.com|news.ebc.net.tw'}
+        pageUrl: {urlMatches: '.*'}
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
--- popup.js
+++ popup.js
@@ -7,22 +7,33 @@ let text4 = document.getElementById('text4');
 let text5 = document.getElementById('text5');
 let text6 = document.getElementById('text6');
 let button = document.getElementById('button');
+let div = document.getElementById('div');
 chrome.storage.local.get(['domain'], async (data) => {
   let domain = data.domain;
   let url = `https://media-expose.wancat.cc/api/media?domain=${domain}`;
   let response = await fetch(url, {method: 'get'});
   let info = await response.json();
-  let media = info[0];
+  let media = (info.length > 0) ? info[0] : {
+    Company: {
+      Name: '',
+      Owner: '',
+      invested_by_china: false,
+    },
+    Name: '',
+    Country: '',
+    ID: '',
+  };
   console.log(media);
   let company = media['Company'];
   text1.innerHTML = domain;
   text2.innerHTML = media['Name'];
-  company['Owner'];
   text3.innerHTML = media['Country'];
   text4.innerHTML = company['Name'];
   text5.innerHTML = company['Owner'];
   text6.innerHTML = company['invested_by_china'] ? '是' : '否';
   button.href = `https://media-expose.wancat.cc/media/${media['ID']}`;
+  if (info.length == 0)
+    div.style.visibility = 'hidden';
 });
 
 // chrome.storage.local.get('author', (data) => {
